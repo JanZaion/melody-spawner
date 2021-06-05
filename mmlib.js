55d492ba598033e69683946f9ca3eeafab01bbee
ZaionCode <zajic815@gmail.com>
2021-06-05 14:15:15 +0200

    add some stuff

--- mmlib.js
+++ mmlib.js
@@ -12,8 +12,51 @@ const {
   Progression,
   Mode,
 } = require('@tonaljs/tonal');
-const { Random } = require('random-js'); //random lib at https://www.npmjs.com/package/random-js
-const random = new Random(); // const value = random.integer(1, 2);
+
+// const { Random } = require('random-js'); //random lib at https://www.npmjs.com/package/random-js //rewrite the old dices to use random in them
+// const random = new Random(); // const value = random.integer(1, 2);
+
+function diceRange(max, min) {
+  //Dice roll, returns any number in the min-max range. Careful: The max number is excluded, so a roll for 2-8 would look like this: diceRange(9, 2)
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function diceMultiRollSortedASC(max, min, rolls) {
+  //Multiple dice rolls, returns an array of ascending different numbers that is as long as the 'rolls' input. Max is excluded just like with diceRange.
+  let arr = [];
+  while (arr.length < rolls) {
+    let r = Math.floor(Math.random() * (max - min) + min);
+    if (arr.indexOf(r) === -1) arr.push(r);
+  }
+  return arr.sort(function (a, b) {
+    return a - b;
+  });
+}
+
+function diceMultiRollSortedDSC(max, min, rolls) {
+  //Multiple dice rolls, returns an array of descending different numbers that is as long as the 'rolls' input. Max is excluded just like with diceRange.
+  let arr = [];
+  while (arr.length < rolls) {
+    let r = Math.floor(Math.random() * (max - min) + min);
+    if (arr.indexOf(r) === -1) arr.push(r);
+  }
+  return arr.sort(function (a, b) {
+    return b - a;
+  });
+}
+
+function diceMultiRollUnsorted(max, min, rolls) {
+  //Multiple dice rolls, returns an array of unsorted numbers that is as long as the 'rolls' input. Max is excluded just like with diceRange.
+  let arr = [];
+  while (arr.length < rolls) {
+    let r = Math.floor(Math.random() * (max - min) + min);
+    if (arr.indexOf(r) === -1) arr.push(r);
+  }
+  return arr;
+}
+
+const a = diceMultiRollUnsorted(1, 5, 3);
+console.log(a);
 
 const humanToBool = (str) => {
   switch (str) {
@@ -85,19 +128,20 @@ const makeMelody = (params) => {
     return lowerMode.concat(upperMode);
   })();
 
+  //we get the # of R notes
+  const numOfRandNotes = (notes.join().match(/R/g) || []).length;
+
   //deciding whether we repeat random notes. If there are more Rs than unused notes in notes array and repeatNotes is false, we declare rep true
   const repeatActually = (() => {
     let preRep = humanToBool(repeatNotes);
     if (preRep === true) return preRep;
 
     const uniqueNotes = [...new Set(notes)]; //deduplication with Set, https://stackoverflow.com/questions/9229645/remove-duplicate-values-from-js-array
-    let notesRemaining = finalMode.length;
+    let notesRemaining = finalMode.length; //abstract away from this fn and make it an array of actual notes
 
     uniqueNotes.forEach((note) => {
       if (finalMode.indexOf(note) !== -1) notesRemaining -= 1;
     });
-
-    const numOfRandNotes = (notes.join().match(/R/g) || []).length;
 
     if (numOfRandNotes > notesRemaining) preRep = true;
 
@@ -107,7 +151,17 @@ const makeMelody = (params) => {
   const finalNotes = (() => {
     switch (repeatActually) {
       case true: // 3 cases, any notes
-        return;
+        switch (pitchDirrection) {
+          case 'any':
+            return diceMultiRollUnsorted(finalMode.length, 0, numOfRandNotes);
+
+          case 'descend':
+            return diceMultiRollSortedASC(finalMode.length, 0, numOfRandNotes);
+
+          case 'ascend':
+            return diceMultiRollSortedDSC(finalMode.length, 0, numOfRandNotes);
+        }
+
       case false: // 3 cases, diff notes
         return;
     }
