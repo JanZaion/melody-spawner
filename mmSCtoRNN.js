003db5135bf7b89ec68146272dd11aaf3c5f50fc
ZaionCode <zajic815@gmail.com>
2021-07-04 16:26:31 +0200

    proon

--- mmSCtoRNN.js
+++ mmSCtoRNN.js
@@ -1,18 +1,7 @@
-// const mvae = require('@magenta/music/node/music_vae');
 const mm = require('@magenta/music/node/music_rnn');
 const core = require('@magenta/music/node/core');
 
-// const process = require('process');
-// const path = require('path');
-// const fs = require('fs');
-
-//2 tested checkpoints:
-// without chord progression: 'https://storage.googleapis.com/magentadata/js/checkpoints/music_rnn/melody_rnn'
-// with chord progression: 'https://storage.googleapis.com/magentadata/js/checkpoints/music_rnn/chord_pitches_improv'
-
-const midiStepsToQuantizedSequence = (midiSteps) => {
-  const { totalDuration, liveFormat } = midiSteps;
-
+const midiStepsToQuantizedSequence = ({ totalDuration, liveFormat }) => {
   const unqunatizedSequence = {
     ticksPerQuarter: 128,
     totalTime: totalDuration,
@@ -55,19 +44,12 @@ const quantizedSequenceToMidiSteps = (quantizedMelody) => {
   return { liveFormat, totalDuration };
 };
 
-const magentize = async (params) => {
-  const {
-    midiSteps,
-    steps = 8,
-    temperature = 1.1,
-    checkpoint = 'https://storage.googleapis.com/magentadata/js/checkpoints/music_rnn/melody_rnn',
-    // checkpoint = 'C:/Users/zajic/Desktop/melody-with-magenta/checkpoints/melody_rnn/',
-    // checkpoint = `${currentPath}`,
-    // checkpoint = currentPath,
-    // checkpoint = path.resolve(process.cwd(), '/checkpoints/melody_rnn'),
-    // checkpoint = './checkpoints/melody_rnn/',
-  } = params;
-
+const magentize = async ({
+  midiSteps,
+  steps = 8,
+  temperature = 1.1,
+  checkpoint = 'https://storage.googleapis.com/magentadata/js/checkpoints/music_rnn/melody_rnn',
+}) => {
   const RNN = new mm.MusicRNN(checkpoint);
   await RNN.initialize();
 
@@ -84,36 +66,3 @@ const magentize = async (params) => {
 };
 
 module.exports = { magentize };
-
-// (async () => {
-//   const asd = await magentize({
-//     scribbleClip: [
-//       { note: ['C2'], length: 256, level: 100 },
-//       { note: ['B2'], length: 256, level: 100 },
-//       { note: ['C2'], length: 256 * 4, level: 100 },
-//       { note: ['B2'], length: 256, level: 100 },
-//     ],
-//     steps: 40,
-//     temperature: 0.4,
-//   });
-//   console.log(asd);
-// })();
-
-// (() => {
-//   console.log(
-//     scribbleClipToQuantizedSequence([
-//       { note: ['C2', 'B2', 'D3'], length: 256, level: 100 },
-//       { note: ['B2'], length: 256, level: 100 },
-//       { note: null, length: 256, level: 100 },
-//       { note: ['C2'], length: 256 * 4, level: 100 },
-//       { note: ['B2'], length: 256, level: 100 },
-//     ])
-//   );
-// })();
-
-// console.log(
-//   asd.reduce((accumulator, step) => {
-//     // the outcome of this must be deterministic
-//     return accumulator + step.length;
-//   }, 0)
-// );
