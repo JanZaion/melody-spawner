15ca7efc9993f011c19ac2f9f9fa439aed038d4a
ZaionCode <zajic815@gmail.com>
2021-08-16 12:57:29 +0200

    fix subdiv

--- makeMelody.js
+++ makeMelody.js
@@ -114,8 +114,8 @@ const scribbleClipToMidiSteps = (scribbleClip) => {
       for (let noteInt = 0; noteInt < step.note.length; noteInt++) {
         liveFormat.push({
           pitch: jsmidgen.Util.midiPitchFromNote(step.note[noteInt]),
-          start_time: startTime / 512,
-          duration: (endTime - startTime) / 512,
+          start_time: startTime / 128,
+          duration: (endTime - startTime) / 128,
           velocity: step.level,
           probability: 1,
           velocity_deviation: 1,
@@ -128,7 +128,7 @@ const scribbleClipToMidiSteps = (scribbleClip) => {
     startTime += step.length;
   }
 
-  const totalDuration = scribbleClip.reduce((duration, step) => (duration = duration + step.length), 0) / 512;
+  const totalDuration = scribbleClip.reduce((duration, step) => (duration = duration + step.length), 0) / 128;
 
   return { liveFormat, totalDuration };
 };
