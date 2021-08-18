0ade62ad5b4c3af2a21130c1c5e8140a2cec3277
ZaionCode <zajic815@gmail.com>
2021-08-18 13:28:32 +0200

    add live 10 support

--- getLive.js
+++ getLive.js
@@ -7,18 +7,54 @@ function clipOrSlot() {
   }
 }
 
-function getLive(dictName) {
-  //Live 11 only, make one for Live 10
-  var clip = clipOrSlot();
+function liveEleven(clip) {
   var end_time = clip.get('end_time');
   var content = clip.call('get_notes_extended', 1, 127, 0, end_time);
   var notes = content.toString();
-  var totalDuration = clip.get('loop_end');
-  var d = new Dict(dictName);
 
-  d.clear();
-  d.parse(notes);
-  d.set('totalDuration', totalDuration); //do we need this?
+  return notes;
+}
+
+function liveTen(clip) {
+  var end_time = clip.get('end_time');
+  var content = clip.call('get_notes', 0, 1, end_time, 127);
+  var notesArr = [];
+  var repeats = content[1] * 5;
+
+  for (var i = 2; i < repeats; i = i + 6) {
+    notesArr.push({
+      pitch: content[i + 1],
+      start_time: content[i + 2],
+      duration: content[i + 3],
+      velocity: content[i + 4],
+    });
+  }
+
+  var notes = JSON.stringify({ notes: notesArr });
+
+  return notes;
+}
+
+function getLive(dictName) {
+  var clip = clipOrSlot();
+  var live = new LiveAPI('live_app');
+
+  if (live.call('get_major_version') === 10) {
+    var notes = liveTen(clip);
+  } else {
+    var notes = liveEleven(clip);
+  }
+
+  var dict = new Dict(dictName);
+  dict.clear();
+  dict.parse(notes);
 
   outlet(0, 'bang');
 }
+
+/*
+to get clip duration and then send it to the dict:
+  var totalDuration = clip.get('loop_end');
+  dict.set('totalDuration', totalDuration); 
+
+*/
